964fe56281786f803d77f0e27e7bbf48a8b06fb5
GitMalo <malo.bonnotte@epfedu.fr>
2023-09-19 13:20:49 +0200

    second commit

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ def hello_world():
  prefix_google = """
  <!-- Google tag (gtag.js) -->
 <script async
-src="https://www.googletagmanager.com/gtag/js?id=UA-285821686-1"></script>
+src="https://www.googletagmanager.com/gtag/js?id=G-GV4KJPM75M"></script>
 <script>
  window.dataLayer = window.dataLayer || [];
  function gtag(){dataLayer.push(arguments);}
  gtag('js', new Date());
- gtag('config', 'UA-285821686-1');
+ gtag('config', 'G-GV4KJPM75M');
 </script>
  """
  return prefix_google + "Hello World"
