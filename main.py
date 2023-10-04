0b31dbd9d6f5e466f1d59fc3afa204ab8ac4fe66
GitMalo <malo.bonnotte@epfedu.fr>
2023-10-04 11:13:06 +0200

    logger

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
- prefix_google = """
+prefix_google = """
  <!-- Google tag (gtag.js) -->
 <script async
 src="https://www.googletagmanager.com/gtag/js?id=G-GV4KJPM75M"></script>
@@ -15,4 +13,22 @@ src="https://www.googletagmanager.com/gtag/js?id=G-GV4KJPM75M"></script>
  gtag('config', 'G-GV4KJPM75M');
 </script>
  """
+
+@app.route("/")
+def hello_world():
  return prefix_google + "Hello World"
+
+@app.route("/logger")
+def logger():
+    # print a message in Python
+    app.logger.info("Connexion à la page Logger")
+
+    # print a message in the browser console
+    browser_log = """
+    <script>
+        console.log('Connexion à la page Logger');
+    </script>
+    """
+
+    return prefix_google + "Logger Page" + browser_log
+
