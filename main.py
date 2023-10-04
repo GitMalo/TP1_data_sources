fddd3470c8dc238e976fe5cc0dd5a19b3fcefe73
GitMalo <malo.bonnotte@epfedu.fr>
2023-10-04 12:56:42 +0200

    text box

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import logging
 
 app = Flask(__name__)
@@ -21,17 +21,39 @@ src="https://www.googletagmanager.com/gtag/js?id=G-GV4KJPM75M"></script>
 def hello_world():
  return prefix_google + "Hello World"
 
-@app.route("/logger")
+@app.route("/logger", methods=["GET", "POST"])
 def logger():
     # print a message in Python
     app.logger.info("Connexion à la page Logger")
 
+    if request.method == 'POST':
+        # take the text in the text box
+        text_from_textbox = request.form['textbox']
+
+    # print a message in the browser console with the text from the text box
+        browser_log = f"""
+        <script>
+            console.log('Connexion à la page Logger');
+            console.log('Texte de la boîte de texte : {text_from_textbox}');
+        </script>
+        """
+
+    else:
     # print a message in the browser console
-    browser_log = """
-    <script>
-        console.log('Connexion à la page Logger');
-    </script>
+        browser_log = """
+        <script>
+            console.log('Connexion à la page Logger');
+        </script>
+        """
+
+    # Formulaire HTML avec une boîte de texte
+    textbox_form = """
+    <form method="POST">
+        <label for="textbox">Text Box :</label><br>
+        <input type="text" id="textbox" name="textbox"><br><br>
+        <input type="submit" value="Soumettre">
+    </form>
     """
 
-    return prefix_google + "Logger Page" + browser_log
+    return "logger page" + browser_log + textbox_form
 
