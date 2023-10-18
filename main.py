aa6ff64d4c163dec0abc12344e1836c1590bef0a
GitMalo <malo.bonnotte@epfedu.fr>
2023-10-18 16:39:29 +0200

    google_trend

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask, request
+from flask import Flask, request, render_template, jsonify
 import logging
 import requests
 import os
 from google.analytics.data_v1beta import BetaAnalyticsDataClient
 from google.analytics.data_v1beta.types import RunReportRequest
+from pytrends.request import TrendReq
 
 app = Flask(__name__)
 
@@ -49,36 +50,8 @@ def logger():
             console.log('Connexion à la page Logger');
         </script>
         """
-
-    # Formulaire HTML avec une boîte de texte
-    textbox_form = """
-    <form method="POST">
-        <label for="textbox">Text Box :</label><br>
-        <input type="text" id="textbox" name="textbox"><br><br>
-        <input type="submit" value="Soumettre">
-    </form>
-    """
-    # Bouton pour effectuer une requête Google
-    google_button = """
-    <form method="GET" action="/perform_google_request">
-        <input type="submit" value="Effectuer la requête Google">
-    </form>
-    """
-    # Bouton pour effectuer une requête Google Analytics
-    google_analytics_button = """
-    <form method="GET" action="/perform_google_analytics_request">
-        <input type="submit" value="Effectuer la requête Google analytics">
-    </form>
-    """
-
-    # Bouton pour récupérer le nombre de visiteurs
-    nombre_visiteurs_button = """
-    <form method="GET" action="/fetch-google-analytics-data">
-        <input type="submit" value="Récupérer le nombre de visiteurs">
-    </form>
-    """
     
-    return "logger page" + browser_log + textbox_form + google_button + google_analytics_button + nombre_visiteurs_button
+    return "logger page" + browser_log + render_template('template_logger.html')
 
 @app.route("/perform_google_request", methods=["GET", "POST"])
 def perform_google_request():
@@ -134,6 +107,21 @@ def fetch_google_analytics_data():
         metric_value = "N/A"
 
     return f'Number of active visitors : {metric_value}'
+
+@app.route('/fetch-google-trend')
+def chart_data():
+    pytrends = TrendReq(hl='en-US', tz=360)
+    keywords = ["football", "basketball"]
+    pytrends.build_payload(keywords, timeframe='today 12-m', geo='US')
+    interest_over_time_df = pytrends.interest_over_time()
+
+    data = {
+        'dates': interest_over_time_df.index.strftime('%Y-%m-%d').tolist(),
+        'football': interest_over_time_df['football'].tolist(),
+        'basketball': interest_over_time_df['basketball'].tolist()
+    }
+
+    return jsonify(data)
 
 if __name__ == '_main_':
     app.run(debug=True)
